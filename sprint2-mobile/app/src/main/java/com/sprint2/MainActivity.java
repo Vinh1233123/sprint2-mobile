63d1ed1b27b0f41fb9f48176436b6cdfec1add10
Administrator <vinhblue89@gmail.com>
2020-12-28 17:09:34 +0700

    vinh_mt 17:09 28/12/2020 kiểm tra kết nối internet và in ra màn hình thông báo

--- sprint2-mobile/app/src/main/java/com/sprint2/MainActivity.java
+++ sprint2-mobile/app/src/main/java/com/sprint2/MainActivity.java
@@ -1,23 +1,41 @@
 package com.sprint2;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnMainLogin;
+    ConnectivityManager connectivityManager;
+    NetworkInfo myWifi, my3G;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mapping();
+        checkNetWork();
         controlBtnMainLogin();
     }
 
+    // Kiểm tra kết nối mạng 3G hoặc wifi
+    private void checkNetWork() {
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        myWifi =  connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        my3G = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (myWifi == null || my3G == null){
+            // Hiển thị ra màn hình thông báo
+            Toast.makeText(getApplicationContext(), "Hãy kiểm tra kết nối mạng", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Set sự kiện khi nhấn login sẽ qua activity login
     private void controlBtnMainLogin() {
         btnMainLogin.setOnClickListener(new View.OnClickListener() {
             @Override
