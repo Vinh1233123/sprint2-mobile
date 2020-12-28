cf29703bb167c6ce7352ec4af42b363b0440b3dc
Administrator <vinhblue89@gmail.com>
2020-12-28 16:29:11 +0700

    vinh_mt 16:29 28/12/2020 đã thiết lập xong nút Đăng nhập ở trang Main

--- sprint2-mobile/app/src/main/java/com/sprint2/MainActivity.java
+++ sprint2-mobile/app/src/main/java/com/sprint2/MainActivity.java
@@ -1,13 +1,34 @@
 package com.sprint2;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnMainLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapping();
+        controlBtnMainLogin();
+    }
+
+    private void controlBtnMainLogin() {
+        btnMainLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void mapping() {
+        btnMainLogin = findViewById(R.id.btnMainLogin);
     }
 }
