043d6ebdc3d0785459f503c39240dc851101ae34
Administrator <vinhblue89@gmail.com>
2020-12-28 16:21:09 +0700

    vinh_mt 16:20 28/12/2020 đã thiết lập xong nút thoát ở trang đăng nhập

--- sprint2-mobile/app/src/main/java/com/sprint2/LoginActivity.java
+++ sprint2-mobile/app/src/main/java/com/sprint2/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.sprint2;
 
+import android.content.DialogInterface;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,11 +12,37 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText edtEmail, edtPassword;
     Button btnLogin, btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mapping();
+        controlBtnBack();
+    }
+
+    private void controlBtnBack() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this, android.R.style.Theme_DeviceDefault_Light_Dialog);
+                builder.setTitle("Bạn có chắc muốn thoát không?");
+                builder.setMessage("Hãy chọn lựa chọn Có hoặc Không");
+                builder.setIcon(android.R.drawable.ic_dialog_alert);
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        onBackPressed();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+            }
+        });
     }
 
     private void mapping() {
